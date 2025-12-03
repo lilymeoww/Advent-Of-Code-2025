24f9d3d34499a46da1aa11f5a688ef70ab493272
Lily Bevan <lilybevan31@gmail.com>
2025-12-03 10:03:33 +0000

    Update main.py

--- 3/main.py
+++ 3/main.py
@@ -1,4 +1,4 @@
-with open("testinput.txt", "r") as inputFile:
+with open("input.txt", "r") as inputFile:
     inputRaw = inputFile.readlines()
     for lineIndex in range(len(inputRaw)):
         inputRaw[lineIndex] = inputRaw[lineIndex].strip()
@@ -17,8 +17,16 @@ for line in inputRaw:
     maxJoltage = ""
     lineList = [int(char) for char in line]
     for i in range(12):
-        maxJoltage += str(max(lineList[:(-12+i)]))
-        lineList = lineList[lineList.index(max(lineList[:(-13+i)]))+1:]
-    print(maxJoltage)
+        if i != 11:
+            maxJoltage += str(max(lineList[:-11+i]))
+            # print(f"chose max {maxJoltage[-1]} from {lineList[:(-11+i)]}, now {maxJoltage}")
+            lineList = lineList[lineList.index(max(lineList[:-11+i])) + 1:]
+            # print(f"New list: {lineList}")
+        else:
+            maxJoltage += str(max(lineList))
+            # print(f"chose max {maxJoltage[-1]} from {lineList}, now {maxJoltage}")
+
+    totalJoltage += int(maxJoltage)
+    # print(maxJoltage)
 
 print(totalJoltage)
