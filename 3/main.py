9c61272428761dd1e8acad1dada582f1f508f580
Lily Bevan <lilybevan31@gmail.com>
2025-12-03 07:29:14 +0000

    day 3 framework

---
+++ 3/main.py
@@ -0,0 +1,4 @@
+with open("input.txt", "r") as inputFile:
+    inputRaw = inputFile.readlines()
+    for lineIndex in range(len(inputRaw)):
+        inputRaw[lineIndex] = inputRaw[lineIndex].strip()
