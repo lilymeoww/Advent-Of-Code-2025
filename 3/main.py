ab76091f15d15404318c391840d3ff485ca1bdd3
Lily Bevan <lilybevan31@gmail.com>
2025-12-03 07:49:11 +0000

    no worky yet

--- 3/main.py
+++ 3/main.py
@@ -1,4 +1,4 @@
-with open("input.txt", "r") as inputFile:
+with open("testinput.txt", "r") as inputFile:
     inputRaw = inputFile.readlines()
     for lineIndex in range(len(inputRaw)):
         inputRaw[lineIndex] = inputRaw[lineIndex].strip()
@@ -9,5 +9,16 @@ for line in inputRaw:
     maxJoltage = int(str(max(lineList[:-1])) + str(max(lineList[lineList.index(max(lineList[:-1])) + 1:])))
     # print(maxJoltage)
     totalJoltage += maxJoltage
+
+print(totalJoltage)
+
+totalJoltage = 0
+for line in inputRaw:
+    maxJoltage = ""
+    lineList = [int(char) for char in line]
+    for i in range(12):
+        maxJoltage += str(max(lineList[:(-12+i)]))
+        lineList = lineList[lineList.index(max(lineList[:(-13+i)]))+1:]
+    print(maxJoltage)
 
 print(totalJoltage)
